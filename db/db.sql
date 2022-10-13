383fbcb55920b8f0722d26fb50a110a456c39775
kakum3 <104210758+kakum3@users.noreply.github.com>
2022-10-13 12:18:10 +0200

    first commit

---
+++ db/db.sql
@@ -0,0 +1,5 @@
+-- Active: 1664291886243@@127.0.0.1@3306@contactsdb
+  CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL
+  );
---
+++ src/Untitled-2.sql
@@ -0,0 +1,5 @@
+-- Active: 1664291886243@@127.0.0.1@3306@contactsdb
+ CREATE TABLE contacts (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL
+  );
